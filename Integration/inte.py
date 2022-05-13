eec8f7170e81e4dc3303747b7265efd5c84a9a64
Balaji VR <40137832+balajivr06@users.noreply.github.com>
2022-05-13 02:55:14 +0100

    Add files via upload

--- Integration/inte.py
+++ Integration/inte.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-# license removed for brevity
+
 
 # [LIBRARIES IMPORT] -------------------------------------------------------------------------
-
 
 ## System imports 
 import sys
@@ -30,10 +29,11 @@ from pal_interaction_msgs.msg import TtsAction, TtsGoal
 
 
 # [ GLOBAL VARIABLES ]-------------------------------------------------------------------------
-global atWaypoint, movementDone, speechDone
+global atWaypoint, movementDone, speechDone, NavCondition
 atWaypoint = False
 movementDone = False
 speechDone =  False
+NavCondition = " "
 #----------------------------------------------------------------------------------------------
 
 
@@ -82,20 +82,24 @@ def callbackNav(state):
 
     global atWaypoint 
     atWaypoint = True
-    rospy.loginfo("I just arrived to the waypoint! ")
+    rospy.loginfo("Navigation Callback: I just arrived to the waypoint! ")
 
 
 # TTS Call back function triggered when action 
 def callbackTts(state, result):
     rospy.loginfo(result)
     rospy.loginfo("Text to Speech just finished with State::" + str(get_status_string(state)) + ", Result: " + str(result))
+    check = str(result)
+    if check == "error_code: -42" :
+	rospy.loginfo("I cannot move my head. Please turn off the head manager!")
+       
 
 ## Call back functions -----------------------------------------------------------------------
 
 
 
-## [Wait function] -----------------------------------------------------------------------
-def wait2Finish(client_kin, client_tts,time2break):
+## [Wait functions] -----------------------------------------------------------------------
+def wait2FinishHRI(client_kin, client_tts,time2break):
     # Get intial state of the action interfaces to compare
     state_kin = client_kin.get_state()
     state_tts = client_tts.get_state()
@@ -138,7 +142,40 @@ def wait2Finish(client_kin, client_tts,time2break):
         rospy.loginfo("Wait2Finish Fucntion_After! State TTS:")
         rospy.loginfo(state_tts)
 
-## -----------------------------------------------------------------------
+
+def wait2FinishNav(time2break):
+    global atWaypoint
+
+    # Timeout limit declaration
+    timeout = rospy.get_time() + time2break
+    timeoutFlag = 0
+    count = 0
+
+    rospy.loginfo("Entered the waiting loop")
+
+    count = 0
+
+    while atWaypoint == False:
+        if count > 50000:
+            rospy.loginfo("On my way there ...")
+            count = 0
+        count = count + 1
+
+        #time out to get out of loop
+        now = rospy.get_time()
+        #rospy.loginfo(now)
+        #rospy.loginfo(timeout)
+	if  now > timeout:
+           rospy.loginfo("Timeout Exception!")
+           timeoutFlag = 1
+	   break
+		
+    if timeoutFlag != 1:
+        rospy.loginfo("Out of the waiting loop")
+    atWaypoint = False
+
+
+## ---------------------------------------------------------------------------------------------
 
 
 ## [Subsystems Functions] -----------------------------------------------------------------------
@@ -146,6 +183,7 @@ def wait2Finish(client_kin, client_tts,time2break):
 
 # Kinematics Function
 def doKinematics(movement,client_kin):
+        global NavCondition
 
         #Create a action goal object based upon the action definition
 	goal_kin = PlayMotionGoal() 
@@ -156,10 +194,13 @@ def doKinematics(movement,client_kin):
         # [Kinematics]  Send action goal
     	rospy.loginfo("Sending goal with motion: " + movement)
     	client_kin.send_goal(goal_kin, callbackKin)
+        
+        #Update position navigation condition
+        NavCondition = movement
 
 
 
-# Text to Speech Function
+# Speaking Function
 def doTts(phrase, waitTime,client_tts):
 
         # Create a goal to say our sentence
@@ -173,6 +214,21 @@ def doTts(phrase, waitTime,client_tts):
         client_tts.send_goal(goal_tts, callbackTts)
 
 
+# Navigation Function
+def doNavigation(waypoint, my_wp, pub_wp, rate):
+
+        # Create a goal for the waypoint
+	my_wp.header.seq = 3
+	my_wp.header.frame_id = 'map'
+	my_wp.goal.poi.data = waypoint
+	rospy.loginfo(my_wp)
+	my_wp.header.stamp = rospy.Time.now()
+
+        # [Navigation] Send the goal
+        rospy.loginfo("I'll go to: " + waypoint)        
+        pub_wp.publish(my_wp)
+	rate.sleep()
+
 ## -----------------------------------------------------------------------
 
 
@@ -180,7 +236,8 @@ def doTts(phrase, waitTime,client_tts):
 
 def integrated():
 
-    global atWaypoint
+    global atWaypoint, NavCondition
+    count = 0
     
     # Node initialization
     rospy.init_node('OneWay', anonymous=True) #this node
@@ -211,38 +268,126 @@ def integrated():
 
 
 
-
-
     ## [Data Definition] ---------------------------------------------------------------------
        
     # Define movements list
-    #movements = ["home","open_hand","wave","thumb_up","wave","look_dt_2","home","point_elevator"]
-    #movements = ["close_hand", "open_hand"]
-    movements = ["wave"]
+    #movements = ["open_hand","wave","wave","look_dt_2","home","thumb_up","close_hand","home"]
+    movements_1 = ["open_hand","wave","thumb_up","wave","look_dt_2","close_hand","home"]
+    movements_2 = ["chin_up","point_elevator","search_below","chin_up","home","close_hand","chin_up"]
+    #movements = ["look_dt_2"]
+
+    # Define time Exceptions
+    timeExceptions_1 = [20, 30, 4, 20, 30, 4, 30]
+    timeExceptions_2 = [6, 40, 40, 10, 30, 5, 10]
+
     
     # Define messages list
-    phrases = ["phrase_1","phrase_2"]
+
+    #phrases = ["phrase 1","phrase 2","phrase 3","phrase 4","phrase 5","phrase 6"]
+    #phrases = ["phrase 1"]
+
+    phrases_1 = ["Hello everybody! It's a pleasure to meet you",
+                 "It is great to see you here.......How are you today?",
+                 "My name is TIAGo",
+                 "I am a Robot",
+                 "Welcome to the Double tree Hotel by Hilton",
+                 "Would you like a drink?",
+                 "Let me get ready to walk you to the Red dot Bar, it'll take just a second"]
+
+    phrases_nav_1 = ["OK", "Please follow me. I will show you the way","Almost there guys"]
+
+    phrases_2 = [ "Okay, we are here",
+                 "This is the Bar. Here you can get a nice drink, ................ I wish I could take a pint of pale oil, but I am not allowed to drink while working",
+                 "Please, get yourselves confortable. One of my robot friends will bring you a drink. Humn? Where is this little guy? It should be around here",
+                 "Anyways, I hope you have a good time",
+                 "I'll just get ready to get back to the reception",
+                 "see you all later",
+		  " "]
+
+
+    phrases_nav_2 = ["OK", "I am going back to work","Almost there"]
+
+
+    # Phrase timings
+    phraseTime_1 = [3, 3, 5, 3, 3, 3, 3]
+    phraseTime_2 = [3, 3, 0, 1, 0, 0, 0]
+    #phraseTime = [3]
 
     # Define waypoint list
-    # XXXXXX
 
-    ## ---------------------------------------------------------------------
+    #waypoints = ["Home","A","B","Home"]
+    waypoints_1 = ["Reception","RedDot"]
+    waypoints_2 = ["RedDot","Reception"]
 
-
-
-    for j in range (0, len(movements)):
-        
-        #Send out Kinematics action Goal
-	doKinematics(movements[j],client_kin)
-
-        #Send out Text to speech action Goal
-	doTts(phrases[j],0,client_tts)
-
-        #Wait for action goals to finish
-    	rospy.loginfo("Waiting for result...")
-        wait2Finish(client_kin,client_tts, 10)
+    ## --------------------------------------------------------------------------------------
 
 
+    ## [Data Execution] ---------------------------------------------------------------------
+
+    #Movements_1
+    if len(movements_1)==len(phrases_1) and len(phraseTime_1)==len(phrases_1) and len(timeExceptions_1)==len(phrases_1):
+	    for j in range (0, len(movements_1)):
+		
+		#Send out Kinematics action Goal
+		doKinematics(movements_1[j],client_kin)
+
+		#Send out Text to speech action Goal
+		doTts(phrases_1[j],phraseTime_1[j],client_tts)
+
+		#Wait for action goals to finish
+	    	rospy.loginfo("Waiting for result...")
+		wait2FinishHRI(client_kin, client_tts, timeExceptions_1[j])
+    else:
+            rospy.loginfo("Number of phrases not equal to number of movements!")
+    
+    #Navigation_1
+    if NavCondition == "home": # Only move if it is at home position, for safety.
+
+	    for i in range (0, len(waypoints_1)):
+		    doTts(phrases_nav_1[i],1,client_tts)
+		    doNavigation(waypoints_1[i], my_wp, pub_wp, rate)
+		    wait2FinishNav(60)
+            NavCondition == " "
+    else:
+            rospy.loginfo("Not in Home (body position), I am not moving man. It is dangerous!")
+            rospy.loginfo(NavCondition)
+
+    #Movements_2
+    if len(movements_2)==len(phrases_2) and len(phraseTime_2)==len(phrases_2) and len(timeExceptions_2)==len(phrases_2):
+	    for j in range (0, len(movements_2)):
+		
+		#Send out Kinematics action Goal
+		doKinematics(movements_2[j],client_kin)
+
+		#Send out Text to speech action Goal
+		doTts(phrases_2[j],phraseTime_2[j],client_tts)
+
+		#Wait for action goals to finish
+	    	rospy.loginfo("Waiting for result...")
+		wait2FinishHRI(client_kin, client_tts, timeExceptions_2[j])
+
+    else:
+            rospy.loginfo("Number of phrases_2 not equal to number of movements!")
+
+
+    #Navigation_2
+
+    NavCondition = "home"
+    if NavCondition == "home": # Only move if it is at home position, for safety.
+
+
+	    for i in range (0, len(waypoints_2)):
+		    doTts(phrases_nav_2[i],1,client_tts)
+		    doNavigation(waypoints_2[i], my_wp, pub_wp, rate)
+		    wait2FinishNav(60)
+            NavCondition == " "
+    else:
+            rospy.loginfo("Not in Home (body position), I am not moving man. It is dangerous!")
+            rospy.loginfo(NavCondition)
+
+
+
+    rospy.loginfo("Program finished successfully")
     rospy.spin()
 
 
